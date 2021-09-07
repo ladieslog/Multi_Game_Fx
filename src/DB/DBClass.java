d52a57157d0012c812a0b93675797bd0eb10913b
matia005 <matia0529@gmail.com>
2021-09-07 13:55:15 +0900

    기본코드 업로드

---
+++ src/DB/DBClass.java
@@ -0,0 +1,18 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBClass {
+	public Connection conn;
+	public DBClass() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn =DriverManager.getConnection("jdbc:oracle:thin:@210.221.253.215:1521:xe","team03","333");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
