1b8991635dac18fc70cb3fcf3c2b1bbd514b3489
ysshin <email@email.com>
2021-09-08 14:31:24 +0900

    1차 업로드

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -4,19 +4,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import NBaseballGame.GameStart;
 
 
 public class Controller implements Initializable{
-	
+	GameStart playball;
 	Parent root;
 	public void setRoot(Parent root) {
 	 	this.root=root;
 	
 	 
  	}
+	public void Playball() {	
+		playball.start();			
+	}
  @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 	// TODO Auto-generated method stub
-	
+	 	playball = new GameStart();
 	}
 }
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,7 +2,6 @@ package main;
 
 import java.io.IOException;
 
-import NBaseballGame.GameStart;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainClass extends Application {
-	GameStart nbaseballgame = new GameStart();
 	public static void main(String[] args) {
 		launch(args);
 	}
