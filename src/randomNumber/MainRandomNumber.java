bc91a5c23291a231b1cc724e7febe2b4d4e5fd72
KJH <wnsgh9978@naver.com>
2021-09-08 14:32:13 +0900

    랜덤게임 기본 창 제작

---
+++ src/randomNumber/MainRandomNumber.java
@@ -0,0 +1,28 @@
+package randomNumber;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainRandomNumber {
+	public void randomNumberGameStart() {
+		randomNumberScene();
+	}
+	
+	private void randomNumberScene() {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("randomNumber.ControllerRandomNumber"));
+			Parent randomRoot = loader.load();
+			
+			ControllerRandomNumber cr = new ControllerRandomNumber();
+			
+			Stage stage = new Stage();
+			stage.setScene(new Scene(randomRoot));
+			
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
