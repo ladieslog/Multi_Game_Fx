ad69897940ac77fc53b1c6f87f2d644ac88f9baf
matia005 <matia0529@gmail.com>
2021-09-10 02:24:38 +0900

    22

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -2,13 +2,20 @@ package main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import TheGame31.Start;
+import TheGame31.TheGameStart;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-
+import Lottery.Start;
+import RockPaperGame.RockMain;
+import randomNumber.MainRandomNumber;
+import NBaseballGame.GameStart;
 
 public class Controller implements Initializable{
-	private Start st =new Start();
+	private TheGameStart st =new TheGameStart();
+	private MainRandomNumber mrn = new MainRandomNumber(); // 랜덤숫자맞추기게임 객체
+	RockMain ro;
+	Start start;
+	GameStart playball;
 	Parent root;
 	public void setRoot(Parent root) {
 	 	this.root=root;	 
@@ -19,9 +26,25 @@ public class Controller implements Initializable{
 	// TODO Auto-generated method stub
 	 
 	}
+ 
  	public void TheGame31() {
  		st.MainBill();
  	}
+ 	
+ 	public void randomNumberGame() {
+ 		mrn.randomNumberGameStart();
+ 	}
+ 	public void rockPaper() {
+		ro.rockPaper();
+	}
+ 	
+ 	public void loterrystart() {
+		start.lotteryGame();
+	}
+ 	public void Playball() {	
+		playball.start();			
+	}
+ 	
  
  
 }
